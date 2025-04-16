6437f2372eff62275beae009bdf5818fcba60c8e
Giulia Polenta <giuliapolenta@gmail.com>
2025-04-16 14:45:51 +0200

    Create grid layout and footer

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,6 +2,7 @@
     <h1 class="site-title"><?= $site->title() ?></h1>
     <nav class="nav">
         <h2 class="site-subtitle">Architecture & Territory</h2>
+        <h3 class="page-title"><?= $page->title() ?></h3>
         <ul class="nav-wrapper">
             <li class="nav-item"><a href="">Works</a></li>
             <li class="nav-item"><a href="">About</a></li>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,6 +1,91 @@
 <?= snippet('head') ?>
 <?= snippet('header') ?>
 
+<main class="main">
+    <section class="section-grid">
+        <div class="grid">
+            <div class="grid-item">
+                <div class="item-header">
+                    <p class="item-header-title --700-text">Some title</p>
+                    <p class="item-header-category --500-text">Category</p>
+                </div>
+                <img src="https://annamariaajmone.com/media/la-notte/la-notte-L-06.jpg" alt="">
+                <div class="item-ui">
+                    <div class="item-ui-counter">
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.99951 12H16.9995M6.99951 12L10.9995 8M6.99951 12L10.9995 16M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <span class="item-ui-counter-text --500-text">1/10</span>
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.0005 12L7.00049 12M17.0005 12L13.0005 16M17.0005 12L13.0005 8M12 0.999999C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 0.999999 12 0.999999Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="item-ui-buttons">
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.94434 11.9553H16.9443M11.9443 6.95526V16.9553M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16.0176 8.14148L8.01758 16.1415M16.0176 8.14148H10.0176M16.0176 8.14148V14.1415M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div class="item-footer">
+                    <p class="item-footer-text --500-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+
+            <div class="grid-item">
+                <div class="item-header">
+                    <p class="item-header-title --700-text">Some title</p>
+                    <p class="item-header-category --500-text">Category</p>
+                </div>
+                <img src="https://annamariaajmone.com/media/la-notte/la-notte-2.jpg" alt="">
+                <div class="item-ui">
+                    <div class="item-ui-counter">
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.99951 12H16.9995M6.99951 12L10.9995 8M6.99951 12L10.9995 16M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <span class="item-ui-counter-text --500-text">1/10</span>
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.0005 12L7.00049 12M17.0005 12L13.0005 16M17.0005 12L13.0005 8M12 0.999999C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 0.999999 12 0.999999Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="item-ui-buttons">
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.94434 11.9553H16.9443M11.9443 6.95526V16.9553M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                        <button>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16.0176 8.14148L8.01758 16.1415M16.0176 8.14148H10.0176M16.0176 8.14148V14.1415M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z" stroke="#001BCB" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div class="item-footer">
+                    <p class="item-footer-text --500-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
 
 <?= snippet('footer') ?>
 <?= snippet('foot') ?>
