dcc988c6ba7ad8668c25028d213eeb26d7d84793
Giulia Polenta <giuliapolenta@gmail.com>
2025-04-17 11:31:48 +0200

    Style project list structure

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -7,37 +7,37 @@
             <div class="list-item">
                 <div class="list-item-wrapper">
                     <ul class="list-item-topbar list-item-topbar-header">
-                        <li class="topbar-label text-label weight-700">
+                        <li class="topbar-label text-label weight-400">
                             <span>Project</span>
                             <svg viewBox="0 0 10 13" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 1L5 11L1 1"/>
                             </svg>
                         </li>
-                        <li class="topbar-label text-label weight-700">
+                        <li class="topbar-label text-label weight-400">
                             <span>Year</span>
                             <svg viewBox="0 0 10 13" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 1L5 11L1 1"/>
                             </svg>
                         </li>
-                        <li class="topbar-label text-label weight-700">
+                        <li class="topbar-label text-label weight-400">
                             <span>Client</span>
                             <svg viewBox="0 0 10 13" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 1L5 11L1 1"/>
                             </svg>
                         </li>
-                        <li class="topbar-label text-label weight-700">
+                        <li class="topbar-label text-label weight-400">
                             <span>Location</span>
                             <svg viewBox="0 0 10 13" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 1L5 11L1 1"/>
                             </svg>
                         </li>
-                        <li class="topbar-label text-label weight-700">
+                        <li class="topbar-label text-label weight-400">
                             <span>Category</span>
                             <svg viewBox="0 0 10 13" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 1L5 11L1 1"/>
                             </svg>
                         </li>
-                        <li class="topbar-label text-label weight-700">
+                        <li class="topbar-label text-label weight-400">
                             <span>Status</span>
                             <svg viewBox="0 0 10 13" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9 1L5 11L1 1"/>
@@ -48,16 +48,16 @@
                 </div>
             </div>
             <?php foreach ($page->children()->listed() as $project) : ?>
-                <div class="list-item" data-project="<?= $project->title() ?>" data-year="<?= $project->year() ?>" data-client="<?= $project->client() ?>" data-location="<?= $project->location() ?>" data-category="<?= $project->category() ?>" data-status="<?= $project->status() ?>">
+                <div class="list-item" data-project="<?= $project->title() ?>" data-year="<?= $project->year() ?>" data-client="<?= $project->client() ?>" data-location="<?= $project->location() ?>" data-category="<?= $project->category() ?>" data-status="<?= $project->stat() ?>">
                     <div class="list-item-wrapper">
-                        <ul class="list-item-topbar">
-                            <li class="topbar-label text-label weight-700"><?= $project->title() ?></li>
-                            <li class="topbar-label text-label weight-700"><?= $project->year() ?></li>
-                            <li class="topbar-label text-label weight-700"><?= $project->client() ?></li>
-                            <li class="topbar-label text-label weight-700"><?= $project->location() ?></li>
-                            <li class="topbar-label text-label weight-700"><?= $project->category() ?></li>
-                            <li class="topbar-label text-label weight-700"><?= $project->status() ?></li>
-                            <li class="topbar-label item-ui-buttons">
+                        <ul class="list-item-topbar list-item-topbar-content">
+                            <li class="topbar-label text-label weight-700"><span><?= $project->title() ?></span></li>
+                            <li class="topbar-label text-label weight-700"><span><?= $project->year() ?></span></li>
+                            <li class="topbar-label text-label weight-700"><span><?= $project->client() ?></span></li>
+                            <li class="topbar-label text-label weight-700"><span><?= $project->location() ?></span></li>
+                            <li class="topbar-label text-label weight-700"><span><?= $project->category() ?></span></li>
+                            <li class="topbar-label text-label weight-700"><span><?= $project->stat() ?></span></li>
+                            <li class="item-ui-buttons">
                                 <button class="button">
                                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M6.94434 11.9553H16.9443M11.9443 6.95526V16.9553M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z"/>
@@ -83,12 +83,13 @@
                                         <figcaption class="text-small"><?= $project->imagecaption()->kt() ?></figcaption>
                                     </figure>
                                 <?php endforeach ?>
+                            </div>
                             <div class="list-item-text">
                                 <div class="list-item-text-wrapper text-large weight-700">
-                                    <?= $project->previewText()->kt() ?>
+                                    <?= $project->description()->kt() ?>
                                 </div>
                                 <a href="<?= $project->url() ?>" class="button">
-                                    <span>View project</span>
+                                    <span class="text weight-700">View project</span>
                                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M16.0176 8.14148L8.01758 16.1415M16.0176 8.14148H10.0176M16.0176 8.14148V14.1415M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23Z"/>
                                     </svg>
