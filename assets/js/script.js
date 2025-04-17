7242049435a811c55319cef6a80d4eed8efcce9f
Giulia Polenta <giuliapolenta@gmail.com>
2025-04-17 13:52:47 +0200

    Add accordion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,16 @@ const documentHeight = () => {
     doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
 };
 
+const headerHeight = () => {
+    const doc = document.documentElement;
+    const header = document.querySelector(".header");
+    doc.style.setProperty("--header-height", `${header.offsetHeight}px`);
+};
+
 const footerHeight = () => {
     const doc = document.documentElement;
     const footer = document.querySelector(".footer");
     doc.style.setProperty("--footer-height", `${footer.offsetHeight}px`);
-    console.log(footer.offsetHeight);
 };
 
 const handleHref = () => {
@@ -25,13 +30,45 @@ const handleHref = () => {
     };
 };
 
+const accordion = () => {
+    const accordion = document.querySelectorAll(".accordion");
+    const allOpeners = document.querySelectorAll(".accordion-opener");
+    const allContent = document.querySelectorAll(".accordion-content");
+    const allPreviews = document.querySelectorAll(".topbar-image");
+    accordion.forEach(item => {
+        const openers = item.querySelectorAll(".accordion-opener");
+        const content = item.querySelector(".accordion-content");
+        const preview = item.querySelector(".topbar-image");
+        openers.forEach(opener => {
+            opener.addEventListener("click", () => {
+                [...allOpeners].filter(i => i !== opener).forEach(i => i.classList.remove("--selected"));
+                [...allContent].filter(i => i !== content).forEach(i => i.classList.remove("--open"));
+                [...allPreviews].filter(i => i !== preview).forEach(i => i.classList.remove("--display"));
+                opener.classList.toggle("--selected");
+                content.classList.toggle("--open");
+                preview.classList.toggle("--display");
+                const offset = 1280;
+                const itemPosition = item.getBoundingClientRect().top;
+                const offsetPosition = itemPosition - offset;
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: "smooth",
+                });
+            });
+        });
+    });
+};
+
 window.addEventListener("load", () => {
     documentHeight();
+    headerHeight();
     footerHeight();
     handleHref();
+    accordion();
 });
 
 window.addEventListener("resize", () => {
     documentHeight();
+    headerHeight();
     footerHeight();
 });
