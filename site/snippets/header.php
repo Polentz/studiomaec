e77eca4605927930de62077dfde15ea816e020bf
Giulia Polenta <giuliapolenta@gmail.com>
2025-06-03 16:07:32 +0200

    Make header content dynamic

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,13 +3,13 @@
 </header>
 <menu class="menu">
     <div class="menu-wrapper">
-        <h2 class="text weight-700">Architecture & Territory</h2>
+        <h2 class="text weight-700"><?= $site->subtitle() ?></h2>
         <h3 class="page-title"><?= $page->title() ?></h3>
         <nav class="nav">
-            <?php foreach($site->children()->listed() as $page) : ?>
+            <?php foreach ($site->children()->listed() as $page) : ?>
                 <a class="nav-item text-label weight-700 link <?= e($page->isOpen(), 'current') ?>" href="<?= $page->url() ?>"><?= $page->title() ?></a>
             <?php endforeach ?>
-            <a class="nav-item text-label weight-700 link js-href" href="#contact">Contact</a>
+            <a class="nav-item text-label weight-700 link js-href" href="#contact"><?= $site->contactname() ?></a>
         </nav>
     </div>
 </menu>
