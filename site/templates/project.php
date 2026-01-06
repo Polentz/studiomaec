6581ec96e23ae8bd04c2c9bc800c8dae2163f5f9
Giulia Polenta <giuliapolenta@gmail.com>
2026-01-06 12:12:51 +0100

    Fix related projects section

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,5 +1,5 @@
 <?php
-$siblings = $page->siblings()->listed();
+$siblings = $page->siblings()->listed()->filterby('publishToggle', true);
 ?>
 
 
@@ -14,19 +14,21 @@ $siblings = $page->siblings()->listed();
     <?= snippet('grid') ?>
 </main>
 
-<section class="related-section">
-    <?php foreach ($siblings->random(1, true) as $sibling) : ?>
-        <div class="button-wrapper">
-            <a href="<?= $sibling->url() ?>" class="button-text weight-700">Next project</a>
-            <a href="<?= $sibling->url() ?>" class="button go">
-                <svg viewBox="0 0 23 22" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M15.5176 7.14148L7.51758 15.1415" />
-                    <path d="M9.51758 7.14148H15.5176V13.1415" />
-                </svg>
-            </a>
-        </div>
-    <?php endforeach ?>
-</section>
+<?php if ($siblings->count() > 1) : ?>
+    <section class="related-section">
+        <?php foreach ($siblings->random(1, true) as $sibling) : ?>
+            <div class="button-wrapper">
+                <a href="<?= $sibling->url() ?>" class="button-text weight-700">Next project</a>
+                <a href="<?= $sibling->url() ?>" class="button go">
+                    <svg viewBox="0 0 23 22" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.5176 7.14148L7.51758 15.1415" />
+                        <path d="M9.51758 7.14148H15.5176V13.1415" />
+                    </svg>
+                </a>
+            </div>
+        <?php endforeach ?>
+    </section>
+<?php endif ?>
 
 <?= snippet('footer') ?>
 <?= snippet('foot') ?>
